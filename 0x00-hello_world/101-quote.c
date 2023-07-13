f034784d73a9efc1c20156a40a2dfefb5b1bdf17
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-13 11:53:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints a quote with write function
+ *       write(int fd, const void *buf, size_t count)
+ * 
+ * Return: 1 (Unsuccess)
+*/
+
+int main(void) 
+{
+	const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote, 59);
+	return (1);
+}

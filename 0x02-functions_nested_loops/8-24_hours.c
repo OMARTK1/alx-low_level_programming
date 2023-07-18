4dc7a538977347fd5812c45ba83108e3fd908d0c
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-18 16:24:52 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,17 @@
 
 void jack_bauer(void)
 {
-	int min, hr;
+	int hr, min;
 
 	for (hr = 0; hr <= 23; hr++)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((hr / 10) + 48);
-			_putchar((hr % 10) + 48);
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + 48);
-			_putchar((hr % 10) + 48);
+			_putchar((min / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar('\n');
 		}
 	}

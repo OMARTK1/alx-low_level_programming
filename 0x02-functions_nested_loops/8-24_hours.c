314cea4bddedc723ccf4a6891622960b33639a77
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-18 12:53:06 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * Jack Bauer: prints every minute of the dayr.
+ * jack_bauer- Prints every minute of the dayr.
 */
 
 void jack_bauer(void)
 {
 	int min, hr;
+
 	for (hr = 0; hr <= 23; hr++)
-	{	
+	{
 		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);

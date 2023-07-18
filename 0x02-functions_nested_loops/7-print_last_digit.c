4ba2cf5cc53f3287b3cd4245007c613e264896bf
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-18 12:20:45 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,16 +12,12 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-		if (n == INT_MIN)
-		{
-			last_digit = n % 10;
-			_putchar(last_digit + '0');
-			return (last_digit);
-		}
-		if (n < 0)
-			n = -n;
+	if (n < 0)
+		last_digit = -1 * (n % 10);
+	else
 		last_digit = n % 10;
-		_putchar(last_digit + '0');
 
-		return (last_digit);
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }

b33184527763f0cc3d77afb4b620048aec92bc84
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-18 18:13:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,18 +2,16 @@
 
 /**
  * print_times_table - Prints the n times table
- * @n: The number for which the times table is printed
  *
- * Return: void
+ * @n: The number imput
 */
 
 void print_times_table(int n)
 {
+	int i, j;
+
 	if (n < 0 || n > 15)
 		return;
-
-	int i;
-	int j;
 
 	for (i = 0; i <= n; i++)
 	{

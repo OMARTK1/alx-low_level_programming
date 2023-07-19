20f448553830be32bbf13e918c14dad148714fe9
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-18 19:08:14 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 	int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
-
+	{
 		for (num = 0; num <= n; num++)
 		{
 			_putchar(48);
@@ -34,6 +34,7 @@ void print_times_table(int n)
 					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
-		printf("\n");
+			_putchar("\n");
 		}
+	}
 }

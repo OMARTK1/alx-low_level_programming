45cda2662779cd714a9a5308a6dc7a0492ebe250
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-24 15:43:24 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line.
+ *
+ * @str: The input string to be printed.
+ */
+
+void _puts(char *str)
+{
+	char *ptr = str
+
+	while (*ptr != '\0')
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+
+	_putchar('\n');
+}

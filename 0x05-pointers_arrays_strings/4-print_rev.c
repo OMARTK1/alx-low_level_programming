f1cc7342a79040ebc3b2590b203c16e4fc76a87f
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-24 16:14:39 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,13 @@ void print_rev(char *s)
 {
 	int len = 0;
 
+	/*Find the length of the string*/
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
+	/*Print characters in reverse order*/
 	while (len > 0)
 	{
 		_putchar(s[len - 1]);

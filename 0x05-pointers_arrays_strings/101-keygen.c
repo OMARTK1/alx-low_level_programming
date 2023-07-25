c1a77dea50484fec7bc83f270d6cfa4bfc21e5bd
OMARTK1 <elyazidi.omar@gmail.com>
2023-07-24 19:17:42 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,19 +6,19 @@
 
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator //
+	char password[PASSWORD_LENGTH + 1]; /* +1 for the null terminator */
 	const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	int i;
 	
-	srand(time(NULL)); // Seed the random number generator with the current time //
+	srand(time(NULL)); /* Seed the random number generator with the current time */
        
-	// Generate random characters for the password //
+	/* Generate random characters for the password */
 	for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
 		password[i] = charset[rand() % (sizeof(charset) - 1)];
 	}
 	
-	password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password string //
+	password[PASSWORD_LENGTH] = '\0'; /* Null-terminate the password string */
 	
 	printf("%s\n", password);
 	

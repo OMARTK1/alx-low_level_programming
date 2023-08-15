ea1c2f87ef8c8d92718db1ed4b6a8aa3b7da667b
OMARTK1 <elyazidi.omar@gmail.com>
2023-08-14 19:19:16 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of dog's owner
+ *
+ * Description: Define a new struct for Poppy dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

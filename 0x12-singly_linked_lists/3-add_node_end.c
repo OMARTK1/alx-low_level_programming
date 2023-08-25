da80f56836a3e8712a631d5e512f9e878707d374
OMARTK1 <elyazidi.omar@gmail.com>
2023-08-24 23:52:25 -0700

    update mandatory

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,7 +8,7 @@
  * Return: A pointer to the new node if successful,
  *		NULL if memory allocation fails
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str);
 {
 	list_t *new_node;
 	list_t *temp = *head;
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (*head == NULL)
 	{
-		*h = new_node;
+		*head = new_node;
 		return (new_node);
 	}
 

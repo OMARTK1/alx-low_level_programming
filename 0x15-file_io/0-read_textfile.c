3fcd30677df03e1a5d5e7983f224abf1b0167afb
OMARTK1 <elyazidi.omar@gmail.com>
2023-09-05 14:50:49 -0700

    updates task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,30 +11,35 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buff[READ_BUFF_SIZE];
+	char *buff;
 	ssize_t re, wr;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+	{
+		free(buff);
+		return (0);
+	}
 
 	re = read(fd, buff, letters);
-	if (re == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	wr = write(STDOUT_FILENO, buff, re);
-	if (wr == -1 || wr != re)
+
+	if (fd == -1 || re == -1 || wr == -1 || wr != re)
 	{
+		free(buff);
 		close(fd);
-		return (-1);
+		return (0);
 	}
 
+	free(buff);
 	close(fd);
+
 	return (wr);
 }

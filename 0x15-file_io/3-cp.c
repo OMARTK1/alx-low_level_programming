464c8d826c1a5c72b98b9e70681866dce49b502f
OMARTK1 <elyazidi.omar@gmail.com>
2023-09-05 19:04:47 -0700

    updates task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define USAGE "Usage: cp cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+
 /**
  * main - Entry point, program copies the contents of a file to another
  * @argc: its the number of arguments
@@ -7,65 +13,37 @@
  *
  * Return: 0 on success, 97, 98, 99, or 100 on error
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int src_fd, dest_fd, cp;
+	int src_fd = 0, dest_fd = 0;
+	ssize_t cp;
+	char buff[READ_BUFF_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 
 	src_fd = open(argv[1], O_RDONLY);
 	if (src_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
 
-	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (dest_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		close(src_fd);
-		return (99);
-	}
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 
-	cp = copy_file(src_fd, dest_fd, buff);
-	close(src_fd);
-	close(dest_fd);
-
+	while ((cp = read(src_fd, buff, READ_BUFF_SIZE)) > 0)
+		if (write(dest_fd, buff, cp) != cp)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 	if (cp == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		return (99);
-	}
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
 
-	return (0);
-}
+	src_fd = close(src_fd);
+	dest_fd = close(dest_fd);
 
-/**
- * copy_file - function that copies the contents of one file to another
- * @src_fd: that is the source file descriptor
- * @dest_fd: that is the destination file descriptor
- * @buff: its a buffer for reading and writing data
- *
- * Return: 0 on success, -1 if its fails
- */
-int copy_file(int src_fd, int dest_fd, char *buff)
-{
-	ssize_t re, wr;
+	if (src_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, src_fd), exit(100);
 
-	while ((re = read(src_fd, buff, READ_BUFF_SIZE)) > 0)
-	{
-		wr = write(dest_fd, buff, re);
-		if (wr == -1 || wr != re)
-			return (-1);
-	}
+	if (dest_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, dest_fd), exit(100);
 
-	if (re == -1)
-		return (-1);
-
-	return (0);
+	return (EXIT_SUCCESS);
 }

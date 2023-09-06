270d860c7890e58de37081da1c9830a6915885ff
OMARTK1 <elyazidi.omar@gmail.com>
2023-09-05 17:45:32 -0700

    update task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,71 @@
 #include "main.h"
 
 /**
- * error_exit - function that prints an error message to STDERR
- *		and exit with a code
- * @message: its the error message to be printed
- * @exit_code: its the exit code to be useed
- *
- * Return: void
- */
-void error_exit(const char *message, int exit_code)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exit_code);
-}
-
-/**
- * main - function that copies the contents of a file to another file
+ * main - Entry point, program copies the contents of a file to another
  * @argc: its the number of arguments
- * @argv: its array of pointers to the arguments
+ * @argv: its an array of arguments
  *
- * Return: 0 on success
+ * Return: 0 on success, 97, 98, 99, or 100 on error
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t re, wr;
-	char buff[READ_BUFF_SIZE];
+	int src_fd, dest_fd, cp;
 
 	if (argc != 3)
 	{
-		error_exit("Usage: cp file_from file_to", 97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
 	{
-		error_exit("Error: Can't read from file", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_to == -1)
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest_fd == -1)
 	{
-		error_exit("Error: Can't write to file", 99);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		close(src_fd);
+		return (99);
 	}
 
-	while ((re = read(fd_from, buff, READ_BUFF_SIZE)) > 0)
+	cp = copy_file(src_fd, dest_fd, buff);
+	close(src_fd);
+	close(dest_fd);
+
+	if (cp == -1)
 	{
-		wr = write(fd_to, buff, re);
-		if (wr == -1)
-		{
-			error_exit("Error: Can't write to file", 99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		return (99);
+	}
+
+	return (0);
+}
+
+/**
+ * copy_file - function that copies the contents of one file to another
+ * @src_fd: that is the source file descriptor
+ * @dest_fd: that is the destination file descriptor
+ * @buff: its a buffer for reading and writing data
+ *
+ * Return: 0 on success, -1 if its fails
+ */
+int copy_file(int src_fd, int dest_fd, char *buff)
+{
+	ssize_t re, wr;
+
+	while ((re = read(src_fd, buff, READ_BUFF_SIZE)) > 0)
+	{
+		wr = write(dest_fd, buff, re);
+		if (wr == -1 || wr != re)
+			return (-1);
 	}
 
 	if (re == -1)
-	{
-		error_exit("Error: Can't read from file", 98);
-	}
-
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		error_exit("Error: Can't close fd", 100);
-	}
+		return (-1);
 
 	return (0);
 }

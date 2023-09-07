1266926bd9bb23e33ac009b54ebf5cd29578bab0
OMARTK1 <elyazidi.omar@gmail.com>
2023-09-07 14:44:12 -0700

    3-cp.c update errors + betty

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void handle_error(const char *msg, const char *arg1, int exit_code);
+int open_file_from(const char *src_file);
+int open_file_to(const char *dst_file);
+void copy_file(int src_fd, int dest_fd);
 
 #endif

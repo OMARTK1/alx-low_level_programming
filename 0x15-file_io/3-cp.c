c8ef5a0207b75e10ae9b8080034a3f0159d38ae8
OMARTK1 <elyazidi.omar@gmail.com>
2023-09-07 15:25:35 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,109 +3,57 @@
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
-#define ERR_NOWRITE "Error: Can't write to file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
-#define ERR_NOREADDEST "Error: Can't read from destination file %s\n"
-#define ERR_NOWRITEDEST "Error: Can't write to destination file %s\n"
+#define ERR_EXISTS "Error: File %s already exists and lacks read permissions\n"
 
 /**
- * handle_error - Prints an error message and exits with an error code.
- * @msg: The error message format.
- * @arg: The argument to include in the error message.
- * @exit_code: The exit code to use when exiting the program.
+ * main - Entry point, program copies the contents of a file to another
+ * @argc: its the number of arguments
+ * @argv: its an array of arguments
  *
- * Return: void
- */
-void handle_error(const char *msg, const char *arg, int exit_code)
-{
-	dprintf(STDERR_FILENO, msg, arg);
-	exit(exit_code);
-}
-
-/**
- * open_file_from - Opens a source file for reading.
- * @src_file: The path to the source file to be opened.
- *
- * Return: The file descriptor of the opened source file.
- */
-int open_file_from(const char *src_file)
-{
-	int src_fd;
-
-	src_fd = open(src_file, O_RDONLY);
-	if (src_fd == -1)
-		handle_error(ERR_NOREAD, src_file, 98);
-
-	return (src_fd);
-}
-
-/**
- * open_file_to - Opens a destination file for writing.
- * @dst_file: The path to the destination file to be opened.
- *
- * Return: The file descriptor of the opened destination file.
- */
-int open_file_to(const char *dst_file)
-{
-	int dest_fd;
-
-	dest_fd = open(dst_file, O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (dest_fd == -1)
-		handle_error(ERR_NOWRITE, dst_file, 99);
-
-	return (dest_fd);
-}
-
-/**
- * copy_file - Copies file contents from source to destination.
- * @src_fd: The file descriptor of the source file.
- * @dest_fd: The file descriptor of the destination file.
- *
- * Return: void
- */
-void copy_file(int src_fd, int dest_fd)
-{
-	ssize_t cp;
-	char buff[READ_BUFF_SIZE];
-
-	while ((cp = read(src_fd, buff, READ_BUFF_SIZE)) > 0)
-	{
-		if (write(dest_fd, buff, cp) != cp)
-			handle_error(ERR_NOWRITEDEST, "file", 99);
-	}
-
-	if (cp < 0)
-		handle_error(ERR_NOREAD, "file", 98);
-}
-
-/**
- * main - Entry point, copies the contents of a file to another.
- * @argc: The number of arguments.
- * @argv: An array of arguments.
- *
- * Return: 1 on success, 0 if it fails.
+ * Return: 1 on success, 0 if it's fails
  */
 int main(int argc, char **argv)
 {
 	int src_fd = 0, dest_fd = 0;
+	ssize_t cp;
+	char buff[READ_BUFF_SIZE];
+	struct stat st;
 
 	if (argc != 3)
-		handle_error(USAGE, "", 97);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	if (stat(argv[2], &st) == 0 && S_ISREG(st.st_mode) &&
+						(st.st_mode & S_IRUSR) == 0)
+	{
+		dprintf(STDERR_FILENO, ERR_EXISTS, argv[2]);
+		close(src_fd);
+		exit(99);
+	}
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (dest_fd == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 
-	src_fd = open_file_from(argv[1]);
-	dest_fd = open_file_to(argv[2]);
+	while ((cp = read(src_fd, buff, READ_BUFF_SIZE)) > 0)
+		if (write(dest_fd, buff, cp) != cp)
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+	if (cp == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
 
-	copy_file(src_fd, dest_fd);
+	src_fd = close(src_fd);
+	dest_fd = close(dest_fd);
+	if (src_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, src_fd), exit(100);
 
-	if (close(src_fd) == -1)
-		handle_error(ERR_NOCLOSE, "src_fd", 100);
-
-	if (close(dest_fd) == -1)
-		handle_error(ERR_NOCLOSE, "dest_fd", 100);
+	if (dest_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, dest_fd), exit(100);
 
 	if (chmod(argv[2], PERMISSIONS) == -1)
-		handle_error("Error: Can't set permissions for %s\n", argv[2], 99);
+		dprintf(STDERR_FILENO, "Error: Can't set permissions for %s\n",
+				argv[2]), exit(99);
 
 	return (EXIT_SUCCESS);
 }
-
